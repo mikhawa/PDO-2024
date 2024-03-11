3daa365e9c0dbb2b3cf67363aaa0ba2c85305e43
WebDevCF2m <gitweb@cf2m.be>
2024-03-11 11:36:24 +0100

    bindValue et execute

--- formateur/prepare/index.php
+++ formateur/prepare/index.php
@@ -11,6 +11,7 @@ Requêtes préparées :
 - réutilisation multiple de la requête
 */
 
+// Premier bindParam avec marqueurs ?
 if(isset($_POST['num1'],$_POST['num2'])):
 
 // Préparation de notre requête avec prepare et les marqueurs ?
@@ -27,7 +28,52 @@ try{
 }
 $resultat = $request->fetchAll();
 
-endif;
+// exécution dans l'execute d'un tableau avec marqueurs nommés 
+$request = $PDOConnect->prepare("SELECT `nom`, `population` FROM `countries` WHERE `population` BETWEEN :debut AND :fin ORDER BY `population` DESC ;");
+
+
+try{
+    $request->execute(["debut"=>$_POST['num1'],
+                        "fin"=>$_POST['num2'],]);
+}catch(Exception $e){
+    die($e->getMessage());
+}
+$resultat2 = $request->fetchAll();
+
+
+// Deuxième bindParam avec marqueurs nommés
+elseif(isset($_POST['sup1'],$_POST['sup2'])):
+
+    // Préparation de notre requête avec prepare et les marqueurs nommés avec :lenom
+    $request = $PDOConnect->prepare("SELECT `nom`, `superficie` FROM `countries` WHERE `superficie` BETWEEN :petit AND :grand ORDER BY `superficie` DESC ;");
+    
+    // utilisation de bindValue(), version plus longue mais plus stricte que le tableau dans l'execute(). 1 représente le premier ? dans l'ordre de lecture (de haut en bas gauche à droite)
+    $request->bindValue(":petit",$_POST['sup1'],PDO::PARAM_INT);
+    $request->bindValue(":grand",$_POST['sup2'],PDO::PARAM_INT);
+    
+    try{
+        $request->execute();
+    }catch(Exception $e){
+        die($e->getMessage());
+    }
+    $resultatSup = $request->fetchAll();
+    
+    // troisième bindParam avec marqueurs ?
+
+    // Préparation de notre requête avec prepare et les marqueurs ?
+    $request = $PDOConnect->prepare("SELECT `nom`, `superficie` FROM `countries` WHERE `superficie` BETWEEN ? AND ? ORDER BY `superficie`;");
+    
+    
+    try{
+        // on peut attribuer des bindValue en mettant un tableau indexé dans l'execute
+        $request->execute([$_POST['sup1'],$_POST['sup2']]);
+
+    }catch(Exception $e){
+        die($e->getMessage());
+    }
+    $resultatSup2 = $request->fetchAll();
+    
+    endif;
 
 ?>
 <!DOCTYPE html>
@@ -56,6 +102,26 @@ endif;
         <p><?=$item['nom']?> | <?=number_format($item['population'],0,'.',' ')?> habitants</p>
             <?php
         endforeach;
+        var_dump($resultat2);
+    endif;
+    ?>
+    <h3>Avec des marqueurs nommés</h3>
+    <h4>Choisissez la superficie du pays entre 2 nombres</h4>
+    <p>Pour afficher les pays avec sa superficie</p>
+    <form action="" method="POST" name="marqueur">
+        <input type="number" name="sup1" value="1" max="10000" step="1000"> à <input type="number" name="sup2" value="20000000" min="100000" step="1000"> <input type="submit" value="rechercher">
+    </form>
+    <?php
+    if(isset($resultatSup)):
+    ?>
+    <h3>Pays ayant entre <?=number_format((int)$_POST['sup1'], 0, '.', ' ');?> et <?=number_format((int)$_POST['sup2'], 0, '.', ' ');?> km2 : <?=count($resultatSup)?></h3>
+    <?php
+        foreach($resultatSup as $item):
+            ?>
+        <p><?=$item['nom']?> | <?=number_format($item['superficie'],0,'.',' ')?> km2</p>
+            <?php
+        endforeach;
+        var_dump($resultatSup2);
     endif;
     ?>
 </body>
