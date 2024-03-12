90e5cbfc8a9155763334fe5732bb70fd361cbc67
mikhawa <michaeljpitz@gmail.com>
2024-03-12 16:26:29 +0100

    GROUP 1

--- formateur/requetes_preparees/menu.php
+++ formateur/requetes_preparees/menu.php
@@ -1,6 +1,7 @@
 <ul>
+    <li><a href="./">Accueil</a></li>
     <li><a href="01-prepare.php">01 - Prepare pour un SELECT simple</a></li>
-    <li><a href=""></a></li>
+    <li><a href="02-prepare-bindValue.php">02 bindValue</a></li>
     <li><a href=""></a></li>
     <li><a href=""></a></li>
     <li><a href=""></a></li>
