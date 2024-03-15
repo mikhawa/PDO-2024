b455a8d7cc76bb8f048b0108b50aba5b65d7c897
WebDevCF2m <gitweb@cf2m.be>
2024-03-15 10:22:22 +0100

    bindParam

---
+++ formateur/requetes_preparees/06-prepare-bindParam-vs-bindValue.php
@@ -0,0 +1,28 @@
+<?php
+require_once "config.php";
+require_once "PDOConnect.php";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Prepare | bindParam vs bindValue</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Prepare | bindParam vs bindValue</h1>
+    <?php include "menu.php" ?>
+    <p>Une requête sans entrés utilisateurs dans un prepare n'a que peu d'intérêt, mis à part la gestion du cache qui peut être améliorée.</p>
+<code>
+    <pre>
+   
+    </pre>
+</code>
+    <?php
+    
+    ?>
+</body>
+</html>
