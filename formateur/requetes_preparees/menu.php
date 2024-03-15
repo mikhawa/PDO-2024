28de047da8cedd64b6866a30cdead98a8797cca8
WebDevCF2m <gitweb@cf2m.be>
2024-03-15 10:02:33 +0100

    execute([]);

--- formateur/requetes_preparees/menu.php
+++ formateur/requetes_preparees/menu.php
@@ -3,7 +3,7 @@
     <li><a href="01-prepare.php">01 - Prepare pour un SELECT simple</a></li>
     <li><a href="02-prepare-bindValue-nommes.php">02 bindValue marqueurs nommés</a></li>
     <li><a href="03-prepare-bindValue-marqueurs.php">03 bindValue marqueurs ?</a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
+    <li><a href="04-prepare-execute-nommes.php">04 execute nommés</a></li>
+    <li><a href="05-prepare-execute-marqueurs.php">05 execute ?</a></li>
     <li><a href=""></a></li>
 </ul>
